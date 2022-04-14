8aa313ae82aab31c4cfafbe76b2cfa31424aecfd
phamdatt3009 <tiendatt3009@gmail.com>
2022-04-14 23:21:40 +0700

    dev-dat

---
+++ MobileApp/MobileApp/MobileAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MobileAppApp.swift
+//  MobileApp
+//
+//  Created by Mr.Donals on 14/04/2022.
+//
+
+import SwiftUI
+
+@main
+struct MobileAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
